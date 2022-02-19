cefdc47ed73e61a62bf6228ae4b6b8fc9a9ccb77
Aviraj Walunj <76213497+avirajwalunj@users.noreply.github.com>
2022-02-19 20:06:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+$('.share').click(function(){
+  // var check = $('.after-share').css.visibility;
+  // alert(check);
+  // // if $('.after-share').attr("class") == '';
+  $('.after-share').toggle().css('visibility','visible');
+})
